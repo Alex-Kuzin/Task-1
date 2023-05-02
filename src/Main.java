2b35ab582d1e3b1fe9f44b591cd1d3debe907b5a
Alex Kuzin <acidkarma3000@gmail.com>
2023-05-02 15:32:32 +0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int price = 3443;
+        int Mile = price/20;
+                System.out.println("Начислено бонусных миль: " + Mile);
+
+        }
+    }
