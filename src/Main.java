3779b15db174a03d27d9f65371294978ae991e8f
Alex Kuzin <acidkarma3000@gmail.com>
2023-05-02 16:19:48 +0500

    Format code

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
     public static void main(String[] args) {
         int price = 3443;
-        int Mile = price/20;
-                System.out.println("Начислено бонусных миль: " + Mile);
+        int Mile = price / 20;
+        System.out.println("Начислено бонусных миль: " + Mile);
 
-        }
     }
+}
