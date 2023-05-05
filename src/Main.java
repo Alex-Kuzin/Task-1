e661efd06a9bbcb1d21bc3f6b5946b633cad5f48
Alex Kuzin <acidkarma3000@gmail.com>
2023-05-05 18:52:18 +0500

    MilesService

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 
 public class Main {
     public static void main(String[] args) {
-        int price = 3443;
-        int Mile = price / 20;
-        System.out.println("Начислено бонусных миль: " + Mile);
-
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price); // должно получиться 500
+        System.out.println(miles);
     }
 }
